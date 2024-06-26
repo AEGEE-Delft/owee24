41edb0a00d28a96b4cbe7c370390b66f83d2d75b
Julius de Jeu <julius@voidcorp.nl>
2024-06-26 12:20:59 +0200

    Index page works with mobile, add English

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    
+    i18n: {
+        defaultLocale: 'nl',
+        locales: ['nl', 'en'],
+    }
 });
